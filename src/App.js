ffc9f076685f289decd85c1c86c8a68a829de2e5
Jackson Buchmeyer <jackson.buchmeyer@drake.edu>
2022-06-24 08:29:53 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Oh boy it works.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Definitely not a rick roll
         </a>
       </header>
     </div>
