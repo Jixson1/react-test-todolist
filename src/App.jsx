3c8e5fe3913a51ffd222d9ce1123c711f8ccf5f3
Jackson Buchmeyer <jackson.buchmeyer@drake.edu>
2022-06-25 15:44:46 -0500

    complete basic todolist app

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,57 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import TodoList from './TodoList';
+import { v4 as uuidv4 } from 'uuid'; // gen random keys for tasks
+
+const LOCAL_STORAGE_KEY = 'todoApp.tasks'
 
 function App() {
   const [tasks, setTasks] = useState([]);
   const taskNameRef = useRef()
 
+  // grabbing stored tasks from local storage
+  useEffect(() => {
+    const storedTasks = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    // only set stored tasks if there are tasks in local storage
+    if (storedTasks) setTasks(storedTasks);
+  }, []);
+
+  // saving tasks to local storage
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(tasks))
+  }, [tasks]); // called when tasks changes
+
+  function toggleTask(id) {
+    // in React, never directly modify state variables, make a copy
+    const newTasks = [...tasks];
+    const task = newTasks.find(task => task.id === id);
+    task.complete = !task.complete;
+    setTasks(newTasks);
+  }
+
+  // when Add Tasks button is pressed
   function handleAddTask(e) {
     const name = taskNameRef.current.value
     if (name === '') return
+    setTasks(prevTasks => {
+      return [...prevTasks, { id: uuidv4(), name: name, complete: false}]
+    })
     taskNameRef.current.value = null;
   }
 
+  // clears tasks that are marked as complete
+  function handleClearTasks() {
+    const newTasks = tasks.filter(task => !task.complete);
+    setTasks(newTasks);
+  }
+
+// component function returns HTML
   return (
     <>
-      <TodoList tasks={tasks} />
+      <TodoList tasks={tasks} toggleTask={toggleTask} />
       <input ref={taskNameRef} type="text" />
       <button onClick={handleAddTask}>Add Tasks</button>
-      <button>Clear Tasks</button>
-      <div>0 Tasks Left</div>
+      <button onClick={handleClearTasks}>Clear Completed</button>
+      <div>{tasks.filter(task => !task.complete).length} Tasks Left</div>
     </>
   );
 }
--- src/Task.jsx
+++ src/Task.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import TodoList from './TodoList';
 
-export default function Task({ task }) {
+export default function Task({ task, toggleTask }) {
+  function handleTaskClick() {
+    toggleTask(task.id);
+  }
+
   return (
     <div>
         <label>
-            <input type="checkbox" checked={task.complete} />
+            <input type="checkbox" checked={task.complete} 
+            onChange={handleTaskClick} />
             {task.name}
         </label>
     </div>
